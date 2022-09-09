26e337488040d0a404114859c46cb6f3fb8c224e
Kyle Jensen <kjensen19@gmail.com>
2022-09-08 21:44:39 -0500

    table and render function built

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,3 +3,17 @@ $(document).ready(onReady);
 function onReady() {
   console.log('JS/JQ')
 }
+
+
+function renderTasks (){
+    console.log('Is this thing on? RENDER')
+    $('#viewTasks').empty();
+    for(let task of tasks){
+      $('#viewTasks').append(`
+        <tr data-id"${task.id}>
+          <td>${task.task}</td>
+          <td>${task.complete}</td>
+        </tr>
+      `);
+    }
+  }
