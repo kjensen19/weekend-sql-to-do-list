245b4ceb55daa311f876f5730878eaf11729eb46
Kyle Jensen <kjensen19@gmail.com>
2022-09-08 20:49:12 -0500

    server setup finished, tested and working

---
+++ server/routes/todo.router.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const toDoRouter = express.Router();
+
+
+// DB CONNECTION
+const db = require('../modules/pool');
+
+
+// GET
+
+// POST
+
+
+// PUT
+
+
+// DELETE
+
+module.exports = toDoRouter;
